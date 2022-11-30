6b13562973610cff578ff8251b21481234995da0
Armando C. Santisbon <3804335+santisbon@users.noreply.github.com>
2022-11-29 23:29:59 -0600

    Clean up downloads

--- containers/docker-build.sh
+++ containers/docker-build.sh
@@ -1,4 +1,4 @@
-# Create a ~/.env file as in the example shown in env.sh
+# Create a ~/.env file like the one in the example shown in env.sh
 # Run this script (docker-build.sh) from the root folder of the GitHub repo
 
 source ./containers/env.sh
@@ -10,16 +10,29 @@ docker build \
   --file ./containers/Dockerfile \
   .
 
+# docker run -it --platform linux/amd64 --name mycontainer santisbon/stablediffusion-amd64
 
-# Default output locations:
-# outputs/txt2img-samples
-# outputs/img2img-samples
+# Download model weights (maybe use a local k8s volume?)
+# https://kubernetes.io/docs/concepts/storage/volumes/#local
+
+mkdir -p ~/Downloads/models/midas_models
+
+# txt2img. Download model weights for SD2.0-v and SD2.0-base. 
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/768-v-ema.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2/resolve/main/768-v-ema.ckpt
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/512-base-ema.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2-base/resolve/main/512-base-ema.ckpt
+
+# img2img. Download the depth-conditional stable diffusion model 
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/512-depth-ema.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2-depth/resolve/main/512-depth-ema.ckpt
+# and the dpt_hybrid MiDaS model weights. Place the dpt_hybrid MiDaS model weights in a folder midas_models.
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/midas_models/dpt_hybrid-midas-501f0c75.pt -nc https://github.com/intel-isl/DPT/releases/download/1_0/dpt_hybrid-midas-501f0c75.pt
+
+# Download upscaling weights
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/x4-upscaler-ema.ckpt -nc  https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler/resolve/main/x4-upscaler-ema.ckpt
+
+# Download the SD 2.0-inpainting checkpoint
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/512-inpainting-ema.ckpt -nc  https://huggingface.co/stabilityai/stable-diffusion-2-inpainting/resolve/main/512-inpainting-ema.ckpt
 
 ########## TXT2IMG ##########
-
-# Download the weights for SD2.0-v and SD2.0-base. 
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc https://huggingface.co/stabilityai/stable-diffusion-2/blob/main/768-v-ema.ckpt
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc https://huggingface.co/stabilityai/stable-diffusion-2-base/resolve/main/512-base-ema.ckpt
 
 # To sample from the SD2.0-v model:
 # python scripts/txt2img.py --prompt "my prompt" --ckpt ./768model.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  
@@ -31,27 +44,18 @@ wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc https://huggingfa
 
 # To augment the well-established img2img functionality of Stable Diffusion, we provide a shape-preserving stable diffusion model.
 # The original method for image modification introduces significant semantic changes w.r.t. the initial image. 
-# If that is not desired, download our depth-conditional stable diffusion model 
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc https://huggingface.co/stabilityai/stable-diffusion-2-depth/resolve/main/512-depth-ema.ckpt
-# and the dpt_hybrid MiDaS model weights, 
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc https://github.com/intel-isl/DPT/releases/download/1_0/dpt_hybrid-midas-501f0c75.pt
-# place the latter in a folder midas_models. WHERE? root folder or in /scripts?
-mkdir midas_models
-mv ./dpt_hybrid-midas-501f0c75.pt midas_models/
-# and sample via
+# This model is particularly useful for a photorealistic style. 
+# For a maximum strength of 1.0, the model removes all pixel-based information and only relies on the text prompt and the inferred monocular depth estimate.
+# Sample via
 # python scripts/gradio/depth2img.py --prompt "photorealistic face with VR headset" --init-img ./some-img.jpg --strength 0.8 configs/stable-diffusion/v2-midas-inference.yaml ./midas_models/dpt_hybrid-midas-501f0c75.pt
 # or 
 # streamlit run scripts/streamlit/depth2img.py --prompt "photorealistic face with VR headset" --init-img ./some-img.jpg configs/stable-diffusion/v2-midas-inference.yaml ./midas_models/dpt_hybrid-midas-501f0c75.pt
-# This model is particularly useful for a photorealistic style. 
-# For a maximum strength of 1.0, the model removes all pixel-based information and only relies on the text prompt and the inferred monocular depth estimate.
 
 # For running the "classic" img2img, use this and adapt the checkpoint (SD2.0-* checkpoints?) and config paths accordingly.
 # python scripts/img2img.py --prompt "A fantasy landscape" --init-img ./some-img.jpg --strength 0.8 --ckpt ./768model.ckpt
 
 ########## UPSCALING ##########
 
-# Download the weights
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc  https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler/resolve/main/x4-upscaler-ema.ckpt
 # For a Gradio or Streamlit demo of the text-guided x4 superresolution model:
 # python scripts/gradio/superresolution.py configs/stable-diffusion/x4-upscaling.yaml ./x4-upscaler-ema.ckpt
 # or
@@ -61,9 +65,11 @@ wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc  https://huggingf
 
 ########## INPAINTING ##########
 
-# Download the SD 2.0-inpainting checkpoint
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -nc  https://huggingface.co/stabilityai/stable-diffusion-2-inpainting/resolve/main/512-inpainting-ema.ckpt
 # For a Gradio or Streamlit demo of the inpainting model:
 # python scripts/gradio/inpainting.py configs/stable-diffusion/v2-inpainting-inference.yaml ./512-inpainting-ema.ckpt
 # or
 # streamlit run scripts/streamlit/inpainting.py -- configs/stable-diffusion/v2-inpainting-inference.yaml ./512-inpainting-ema.ckpt
+
+# Default output locations:
+# outputs/txt2img-samples
+# outputs/img2img-samples
