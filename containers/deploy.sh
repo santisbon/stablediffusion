4f6e233edd9b43e3129b8368183b24bb8f24d217
Armando C. Santisbon <3804335+santisbon@users.noreply.github.com>
2022-12-10 18:31:57 -0600

    Get SD 2.1 models

--- containers/deploy.sh
+++ containers/deploy.sh
@@ -10,10 +10,21 @@ docker build \
 
 docker push "${STABLEDIFFUSION_TAG}"
 
+# Find the links:
+# grep -i https://huggingface.co/stabilityai/stable-diffusion README.md > containers/links.txt
+# grep -i https://github.com/intel-isl/DPT/releases/download README.md > containers/links.txt
+
+# https://huggingface.co/stabilityai/stable-diffusion-2-1
+# https://huggingface.co/stabilityai/stable-diffusion-2-1-base
+# https://huggingface.co/stabilityai/stable-diffusion-2-depth
+# https://github.com/intel-isl/DPT/releases/download/1_0/dpt_hybrid-midas-501f0c75.pt
+# https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler
+# https://huggingface.co/stabilityai/stable-diffusion-2-inpainting
+
 mkdir -p ~/Downloads/models/midas_models
-# Download model weights for SD2.0-v and SD2.0-base (txt2img).
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/768-v-ema.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2/resolve/main/768-v-ema.ckpt
-wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/512-base-ema.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2-base/resolve/main/512-base-ema.ckpt
+# Download model weights for _SD2.1-v_ and _SD2.1-base_ (txt2img).
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/v2-1_768-ema-pruned.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2-1/blob/main/v2-1_768-ema-pruned.ckpt
+wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/v2-1_512-ema-pruned.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v2-1_512-ema-pruned.ckpt
 # Download the depth-conditional stable diffusion model (img2img) 
 wget --header="Authorization: Bearer ${HUGGINGFACE_TOKEN}" -O ~/Downloads/models/512-depth-ema.ckpt -nc https://huggingface.co/stabilityai/stable-diffusion-2-depth/resolve/main/512-depth-ema.ckpt
 # and the dpt_hybrid MiDaS model weights. Place the dpt_hybrid MiDaS model weights in a folder midas_models.
